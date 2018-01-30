6f90e6c572e917bb89d1057b62f28b422221c8fe
Raphaël <raphael.godart@outlook.com>
2018-01-30 20:34:35 +0100

    Added font icons

---
+++ Digimezzo.Foundation.WPF/Base/FontIconBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Digimezzo.Foundation.WPF.Base
+{
+    public class FontIconBase : Control
+    {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(FontIconBase), new PropertyMetadata(null));
+
+        static FontIconBase()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FontIconBase), new FrameworkPropertyMetadata(typeof(FontIconBase)));
+        }
+    }
+}
---
+++ Digimezzo.Foundation.WPF/Controls/FontIcons.cs
@@ -0,0 +1,29 @@
+﻿using Digimezzo.Foundation.WPF.Base;
+using System.Windows;
+
+namespace Digimezzo.Foundation.WPF.Controls
+{
+    public class FAIcon : FontIconBase
+    {
+        static FAIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FAIcon), new FrameworkPropertyMetadata(typeof(FAIcon)));
+        }
+    }
+
+    public class SegoeIcon : FontIconBase
+    {
+        static SegoeIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SegoeIcon), new FrameworkPropertyMetadata(typeof(SegoeIcon)));
+        }
+    }
+
+    public class MaterialIcon : FontIconBase
+    {
+        static MaterialIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MaterialIcon), new FrameworkPropertyMetadata(typeof(MaterialIcon)));
+        }
+    }
+}
