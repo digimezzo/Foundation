369ff4055e16291bb70a65bce7d59dc4d284c013
Raphaël <raphael.godart@outlook.com>
2018-05-15 06:55:27 +0200

    Added SplitWords

--- Digimezzo.Foundation.Core/Utils/StringUtils.cs
+++ Digimezzo.Foundation.Core/Utils/StringUtils.cs
@@ -15,5 +15,19 @@ namespace Digimezzo.Foundation.Core.Utils
             if (String.IsNullOrEmpty(input)) return input;
             return input.First().ToString().ToUpper() + input.Substring(1);
         }
+
+        /// <summary>
+        /// Splits strings on spaces, but preserves strings which are between double quotes.
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static string[] SplitWords(string inputString)
+        {
+            return inputString.Split('"')
+                     .Select((element, index) => index % 2 == 0  // If even index
+                                           ? element.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)  // Split the item
+                                           : new string[] { element })  // Keep the entire item
+                     .SelectMany(element => element).ToArray();
+        }
     }
 }
